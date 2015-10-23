516b38a9644bf01533492289f83308923ef6a2df
Akash Shakdwipeea <ashakdwipeea@gmail.com>
2015-10-23 21:46:14 +0530

    layout pushed

--- app/src/main/java/com/urbanmechanik/urbanmechanicvendor/BookingListFragment.java
+++ app/src/main/java/com/urbanmechanik/urbanmechanicvendor/BookingListFragment.java
@@ -8,6 +8,7 @@ import android.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
@@ -39,12 +40,19 @@ public class BookingListFragment extends Fragment {
         // Required empty public constructor
     }
 
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_booking_list, container, false);
+
 
         getBookings();
 
@@ -61,6 +69,18 @@ public class BookingListFragment extends Fragment {
         Toast.makeText(getActivity(), "Booking list fragment", Toast.LENGTH_SHORT).show();
         return v;
 
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_refresh: {
+                getBookings();
+                return true;
+            }
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     public void getBookings () {
@@ -80,6 +100,7 @@ public class BookingListFragment extends Fragment {
             public void onResponse(JSONObject response) {
                 try {
                     JSONArray bookings = response.getJSONArray("bookings");
+                    mBooking.clear();
 
                     for (int i = 0; i < bookings.length(); i++) {
                         JSONObject booking_object = bookings.getJSONObject(i);
